de1014cd8ad8aae79f85299e5e88333423d115b2
りんご飴196 <kazumac41@gmail.com>
2025-05-18 13:09:44 +0900

    ✨ Data追加

---
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/Data.kt
@@ -0,0 +1,5 @@
+package com.github.ringoame196_s_mcPlugin
+
+object Data {
+    var gameStatus: Boolean = false
+}
--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/Main.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/Main.kt
@@ -1,7 +1,7 @@
 package com.github.ringoame196_s_mcPlugin
 
 import com.github.ringoame196_s_mcPlugin.commands.Command
-import com.github.ringoame196_s_mcPlugin.events.Events
+import com.github.ringoame196_s_mcPlugin.events.GunEvent
 import org.bukkit.plugin.java.JavaPlugin
 
 class Main : JavaPlugin() {
